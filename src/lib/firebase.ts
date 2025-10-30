80c0918f9b37196e36d66b8dcc79f775209ee8cd
Noah Enemali <noahbig001@gmail.com>
2025-10-30 17:45:09 +0000

    small fix

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,16 +4,16 @@ import { getFirestore } from "firebase/firestore";
 
 // This is a public configuration and is safe to expose.
 const firebaseConfig: FirebaseOptions = {
-    apiKey: "your-api-key",
-    authDomain: "your-auth-domain",
-    projectId: "your-project-id",
-    storageBucket: "your-storage-bucket",
-    messagingSenderId: "your-messaging-sender-id",
-    appId: "your-app-id",
+    apiKey: "AIzaSyBTqdhX9SwrExSraPDBsMa4naNlitSHw3U",
+    authDomain: "spooky-punks.firebaseapp.com",
+    projectId: "spooky-punks",
+    storageBucket: "spooky-punks.appspot.com",
+    messagingSenderId: "802572131043",
+    appId: "1:802572131043:web:026706bc4c8a7759ba83d6",
+    measurementId: "G-JCS8QEXJ2D"
 };
 
-let app, auth, db;
-
+let app;
 // This pattern ensures that Firebase is initialized only once.
 if (getApps().length === 0) {
   app = initializeApp(firebaseConfig);
@@ -23,8 +23,8 @@ if (getApps().length === 0) {
 
 // These services can be initialized here and reused throughout the app.
 // It's safe to call them multiple times as they will return the same instance.
-auth = getAuth(app);
-db = getFirestore(app);
+const auth = getAuth(app);
+const db = getFirestore(app);
 
 
 export { app, auth, db };
