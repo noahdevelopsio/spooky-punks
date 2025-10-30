7e17ccb81553d7dc89f8aab302580464fe6b63c0
Noah Enemali <noahbig001@gmail.com>
2025-10-30 17:47:42 +0000

    Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4654518983-f5fb5",
+  "appId": "1:395796409411:web:04b38b557b24e3ae0ef8f6",
+  "apiKey": "AIzaSyDsxLFWTCxdLkdGMpvb0QHnpOphNWbjiFs",
+  "authDomain": "studio-4654518983-f5fb5.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "395796409411"
+};
