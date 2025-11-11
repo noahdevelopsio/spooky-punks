95c66ef28333b64471c3609b0322866a127724b0
Noah Enemali <noahbig001@gmail.com>
2025-11-11 20:23:17 +0000

    imagen-4.0-generate-001

--- src/ai/flows/generate-punk-flow.ts
+++ src/ai/flows/generate-punk-flow.ts
@@ -84,7 +84,7 @@ const generatePunkFlow = ai.defineFlow(
     `;
 
     const {media} = await ai.generate({
-      model: 'googleai/gemini-2.0-flash-preview-image-generation',
+      model: 'googleai/imagen-4.0-generate-001',
       prompt: [
         {text: textPrompt},
         ...mediaParts
