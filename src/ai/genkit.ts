f0fc1fe830eb058d795694ba95f2b96516324d0f
Noah Enemali <noahbig001@gmail.com>
2025-11-25 15:09:37 +0000

    Now imort and use vertex ai, in genkit.ts

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,9 +1,11 @@
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/google-genai';
+import {vertexAI} from '@genkit-ai/vertexai';
 
 export const ai = genkit({
   plugins: [
     googleAI(),
+    vertexAI(),
   ],
   model: 'googleai/gemini-2.5-flash',
 });
