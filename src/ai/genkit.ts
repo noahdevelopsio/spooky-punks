67aacc7282b73f9ea49767caedb4fbcac2f1821c
Noah Enemali <noahbig001@gmail.com>
2025-11-25 15:04:56 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve '@g

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,14 +1,9 @@
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/google-genai';
-import {vertexAI} from '@genkit-ai/vertexai';
 
 export const ai = genkit({
   plugins: [
     googleAI(),
-    vertexAI({
-      projectId: 'studio-4654518983-f5fb5',
-      location: 'us-central1',
-    }),
   ],
   model: 'googleai/gemini-2.5-flash',
 });
